d7fa97412f6763f81e8f02118290215a454dc854
Eugene-Forest <1244303915@qq.com>
2024-10-29 10:51:05 +0800

    refactor: 添加 测试用例

--- BotWeb/src/test/java/org/tutor/botweb/BotWebApplicationTests.java
+++ BotWeb/src/test/java/org/tutor/botweb/BotWebApplicationTests.java
@@ -1,13 +1,30 @@
 package org.tutor.botweb;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.tutor.botweb.demos.DemoControl;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class BotWebApplicationTests {
 
+//    @Autowired
+//    DemoControl demoControl;
+
+    @Autowired
+    MockMvc mockMvc;
+
     @Test
-    void contextLoads() {
+    void contextLoads() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/demo/getMessage"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk());
     }
 
 }
