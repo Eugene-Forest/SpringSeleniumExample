50568ab4cf77ae2aff9632ba980b8640368c08e6
Eugene-Forest <1244303915@qq.com>
2024-11-13 14:00:54 +0800

    feat：调整项目结构3

---
+++ TutorDataSource/src/main/java/org/tutor/tutordatasource/TutorDataSourceApplication.java
@@ -0,0 +1,17 @@
+package org.tutor.tutordatasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Eugene-Forest
+ * {@code @date} 2024/11/13
+ * {@code @project} TuTorSelenium
+ */
+@SpringBootApplication
+public class TutorDataSourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TutorDataSourceApplication.class, args);
+    }
+}
---
+++ TutorDataSource/src/test/java/TestTutorDataSourceApp.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author Eugene-Forest
+ * {@code @date} 2024/11/13
+ * {@code @project} TuTorSelenium
+ */
+@SpringBootTest
+public class TestTutorDataSourceApp {
+
+    @Test
+    public void contextLoads() {}
+}
