cf873e6c1eacae820db619563e16db90c5ae464c
Eugene-Forest <1244303915@qq.com>
2024-11-18 18:18:41 +0800

    test: 添加测试代码

---
+++ BotWeb/src/test/java/org/tutor/botweb/NacosSecretUtil.java
@@ -0,0 +1,25 @@
+package org.tutor.botweb;
+
+import org.springframework.util.Base64Utils;
+
+import java.nio.charset.StandardCharsets;
+
+/**
+ * <p>
+ * nacos中JWT令牌密钥生成器<br>
+ * 生成 nacos.core.auth.plugin.nacos.token.secret.key 的值
+ * </p>
+ * @author shigzh
+ * @since 2024/1/10 13:41
+ */
+public class NacosSecretUtil {
+    public static void main(String[] args) {
+        // 自定义生成JWT令牌的密钥
+        String nacosSecret = "nacos_secret_tencent_eugene_forest";
+        // 输出密钥长度，要求不得低于32字符，否则无法启动节点。
+        System.out.println("密钥长度》》》" + nacosSecret.length());
+        // 密钥进行Base64编码
+        byte[] data = nacosSecret.getBytes(StandardCharsets.UTF_8);
+        System.out.println("密钥Base64编码》》》" + Base64Utils.encodeToString(data));
+    }
+}
