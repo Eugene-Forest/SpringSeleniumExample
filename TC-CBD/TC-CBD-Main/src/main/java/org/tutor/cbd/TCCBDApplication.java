fbe3fefb9358175a70112b81fcbdba855854fbf7
Eugene-Forest <1244303915@qq.com>
2024-11-19 11:10:09 +0800

    refactor: 添加SpringCloud项目

---
+++ TC-CBD/TC-CBD-Main/src/main/java/org/tutor/cbd/TCCBDApplication.java
@@ -0,0 +1,15 @@
+package org.tutor.cbd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Eugene-Forest
+ * {@code @date} 2024/11/19
+ */
+@SpringBootApplication
+public class TCCBDApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TCCBDApplication.class, args);
+    }
+}
