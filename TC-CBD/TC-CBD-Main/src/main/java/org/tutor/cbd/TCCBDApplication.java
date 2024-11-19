c347a2d5440335394a65e5d61767bfab9942dcf5
Eugene-Forest <1244303915@qq.com>
2024-11-19 15:51:24 +0800

    refactor: SpringCloud 项目的Nacos注册与发现服务配置

--- TC-CBD/TC-CBD-Main/src/main/java/org/tutor/cbd/TCCBDApplication.java
+++ TC-CBD/TC-CBD-Main/src/main/java/org/tutor/cbd/TCCBDApplication.java
@@ -2,12 +2,14 @@ package org.tutor.cbd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author Eugene-Forest
  * {@code @date} 2024/11/19
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class TCCBDApplication {
     public static void main(String[] args) {
         SpringApplication.run(TCCBDApplication.class, args);
