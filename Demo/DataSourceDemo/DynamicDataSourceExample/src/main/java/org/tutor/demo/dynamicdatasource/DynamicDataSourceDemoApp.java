4e28dd3362785b2ab8aefebdaea7c95146dbf26b
Eugene <1244303915@qq.com>
2024-11-24 19:27:51 +0800

    refactor：调整项目结构

---
+++ Demo/DataSourceDemo/DynamicDataSourceExample/src/main/java/org/tutor/demo/dynamicdatasource/DynamicDataSourceDemoApp.java
@@ -0,0 +1,16 @@
+package org.tutor.demo.dynamicdatasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author qaz22
+ * {@code @date} 2024/11/24
+ * {@code @project} tutor-selenium
+ */
+@SpringBootApplication
+public class DynamicDataSourceDemoApp {
+    public static void main(String[] args) {
+        SpringApplication.run(DynamicDataSourceDemoApp.class, args);
+    }
+}
---
+++ Demo/DataSourceDemo/MultipleDataSourceExample/src/main/java/org/tutor/demo/multipledatasource/MultipleDataSourceExampleApp.java
@@ -0,0 +1,16 @@
+package org.tutor.demo.multipledatasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author qaz22
+ * {@code @date} 2024/11/24
+ * {@code @project} tutor-selenium
+ */
+@SpringBootApplication
+public class MultipleDataSourceExampleApp {
+    public static void main(String[] args) {
+        SpringApplication.run(MultipleDataSourceExampleApp.class, args);
+    }
+}
